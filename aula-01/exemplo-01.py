d705e208d55e9a69aad3f4b95192708134f7a027
DSM3 <fatec-dsm3@academico.intra>
2024-08-05 20:19:34 -0300

    subindo...

---
+++ aula-01/exemplo-01.py
@@ -0,0 +1,5 @@
+valor1 = int(input("Digite o valor 1: "))
+valor2 = int(input("Digite o valor 2: "))
+
+soma = valor1 + valor2
+
---
+++ aula-01/exemplo-02.py
@@ -0,0 +1,6 @@
+base = float(input("Digite o valor da base: "))
+altura = float(input("Digite o valor da altura: "))
+area = base * altura / 2
+
+#para arredondar valor utilizamos :.2f
+print(f"O valor da área é de: {area:.2f} cm²")
